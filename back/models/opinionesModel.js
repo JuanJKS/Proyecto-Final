196c8270afc51d40068264703bd69d59bfdc49c5
Juan Cruz Bo <bo_juancruz@hotmail.com>
2022-11-15 00:56:13 -0300

    Cambios estilos back

---
+++ back/models/opinionesModel.js
@@ -0,0 +1,44 @@
+var pool = require('./bd');
+
+async function getOpiniones() {
+    var query = 'SELECT * FROM opiniones';
+    var rows = await pool.query(query);
+    return rows;
+}
+
+async function insertOpinion(obj) {
+    try {
+        var query = 'INSERT INTO opiniones SET ?';
+        var rows = await pool.query(query, [obj]);
+        return rows;
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
+
+async function deleteOpinionById(id) {
+    var query = 'DELETE FROM opiniones WHERE id_op = ?';
+    var rows = await pool.query(query, [id]);
+    return rows;
+}
+
+/*TRAE NOVEDAD POR ID PARA LUEGO EDITARLA*/
+async function getOpinionById(id) {
+    var query = 'SELECT * FROM opiniones WHERE id_op = ?';
+    var rows = await pool.query(query, [id]);
+    return rows[0];
+}
+
+/*ACTUALIZAR LA OPINIÓN*/
+async function modificarOpinionById(obj, id) {
+    try {
+      var query = 'UPDATE opiniones SET ? WHERE id_op = ?';
+      var rows = await pool.query(query, [obj, id]);
+      return rows;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+module.exports = { getOpiniones, insertOpinion, deleteOpinionById, getOpinionById, modificarOpinionById };
--- back/routes/admin/opiniones.js
+++ back/routes/admin/opiniones.js
@@ -1,12 +1,87 @@
 var express = require('express');
 var router = express.Router();
+var opinionesModel = require('../../models/opinionesModel');
 
 /* GET opiniones page. */
-router.get('/', function(req, res, next) {
+router.get('/', async function(req, res, next) {
+
+  var opiniones = await opinionesModel.getOpiniones();
+
   res.render('admin/opiniones', {
     layout: 'admin/layout',
-    persona: req.session.nombre_usuario
+    persona: req.session.nombre_usuario,
+    opiniones
   });
+});
+
+/*DISEÑO DE AGREGAR*/
+router.get('/agregar', (req, res, next) => {
+  res.render('admin/agregar', {
+    layout: 'admin/layout'
+  });
+});
+
+/*INSERTAR LA NOVEDAD*/
+router.post('/agregar', async (req, res, next) => {
+  try {
+    if (req.body.opinion != "" && req.body.autor != "" && req.body.zona != "") {
+      await opinionesModel.insertOpinion(req.body);
+      res.redirect('/admin/opiniones');
+    } else {
+      res.render('admin/agregar', {
+        layout: 'admin/layout',
+        error: true,
+        message: 'Todos los campos son requeridos'
+      })
+    }
+  } catch (error) {
+      console.log(error);
+      res.render('admin/agregar', {
+        layout: 'admin/layout',
+        error: true,
+        message: 'No se cargo la novedad'
+      })
+  }
+});
+
+/*ELIMINAR LA OPINIÓN*/
+router.get('/eliminar/:id', async (req, res, next) => {
+  var id = req.params.id;
+  await opinionesModel.deleteOpinionById(id);
+  res.redirect('/admin/opiniones');
+});
+
+/*ME TRAE LA VISTA PARA LA MODIFICACIÓN; FORMULARIO CON LOS DATOS CARGADOS DE LA OPINION A EDITAR*/
+router.get('/modificar/:id', async (req, res, next) => {
+  var id = req.params.id;
+  var opinion = await opinionesModel.getOpinionById(id);
+  res.render('admin/modificar', {
+    layout: 'admin/layout',
+    opinion
+  })
+});
+
+/*ACTUALIZAR LA OPINIÓN*/
+router.post('/modificar', async (req, res, next) => {
+  try {
+    var obj = {
+      opinion: req.body.opinion,
+      autor: req.body.autor,
+      zona: req.body.zona
+    };
+    //console.log(obj);
+    //console.log(req.body.id);
+
+    await opinionesModel.modificarOpinionById(obj, req.body.id);
+    res.redirect('/admin/opiniones');
+  } catch (error) {
+    console.log(error);
+    res.render('admin/modificar', {
+      layout: 'admin/layout',
+      error: true,
+      message: 'No se modifico la novedad'
+    })
+  }
 });
 
 module.exports = router;
