6398b54bfa882a5856be996a0cddef2036274aa5
Juan Cruz Bo <bo_juancruz@hotmail.com>
2022-12-02 18:51:06 -0300

    Cambio 02/12

--- front/src/pages/MisClasesPage.js
+++ front/src/pages/MisClasesPage.js
@@ -14,7 +14,7 @@ const MisClasesPage = (props) => {
     useEffect(() => {
         const cargarOpiniones = async () => {
             setLoading(true);
-            const response = await axios.get('http://localhost:3000/api/opiniones');
+            const response = await axios.get('http://backend:3000/api/opiniones');
             setOpiniones(response.data);
             setLoading(false);
         };
